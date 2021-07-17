cde9a2e279b452e2682ee094e85bf89cf9362560
arthmrnd <theodinksson@gmail.com>
2021-07-17 20:26:09 -0300

    Add post method on /jogador

--- src/main/java/br/com/letscode/moviesbattle/jogador/JogadorRestController.java
+++ src/main/java/br/com/letscode/moviesbattle/jogador/JogadorRestController.java
@@ -1,6 +1,7 @@
 package br.com.letscode.moviesbattle.jogador;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
-@RequestMapping("/login")
+@RequestMapping("/jogador")
 @RestController
 public class JogadorRestController {
 
+    private final JogadorRestRepository jogadorRestRepository;
+
+    public JogadorRestController(JogadorRestRepository jogadorRestRepository) {
+        this.jogadorRestRepository = jogadorRestRepository;
+    }
+
     @GetMapping
     public String login(@RequestBody Jogador jogador) throws NoSuchAlgorithmException, IOException {
-        JogadorRestRepository jogadorRestRepository = new JogadorRestRepository();
+        jogadorRestRepository.loadJogadoresList();
         if(jogadorRestRepository.compareJogador(jogador.getUser(), jogador.getPassword())){
             return "Login bem sucedido";
         }
@@ -23,4 +30,13 @@ public class JogadorRestController {
         }
     }
 
+    @PostMapping
+    public String cadastroJogador(@RequestBody Jogador jogador) throws IOException, NoSuchAlgorithmException {
+        if (jogadorRestRepository.createJogador(jogador)){
+            return "Jogador criado";
+        }
+        else {
+            return "ERRO";
+        }
+    }
 }
--- src/main/java/br/com/letscode/moviesbattle/jogador/JogadorRestRepository.java
+++ src/main/java/br/com/letscode/moviesbattle/jogador/JogadorRestRepository.java
@@ -1,8 +1,11 @@
 package br.com.letscode.moviesbattle.jogador;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import org.springframework.stereotype.Component;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -10,12 +13,14 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class JogadorRestRepository {
     List<Jogador> jogadoresList;
+    private final String path = "src/main/resources/dados/jogadores.csv";
 
-    public JogadorRestRepository() throws IOException {
+    public void loadJogadoresList() throws IOException {
         jogadoresList = new ArrayList<>();
-        CSVReader reader = new CSVReader(new FileReader("jogadores.csv"));
+        CSVReader reader = new CSVReader(new FileReader(path));
         String[] line;
         while ((line = reader.readNext()) != null) {
             var jogador = new Jogador();
@@ -26,16 +31,51 @@ public class JogadorRestRepository {
     }
 
     public Boolean compareJogador(String user, String password) throws NoSuchAlgorithmException {
-        MessageDigest algorithm = MessageDigest.getInstance("SHA-1");
-        byte[] passwordHash = algorithm.digest(password.getBytes(StandardCharsets.UTF_8));
-
-        var passwordHex = new StringBuilder();
-        for (byte b : passwordHash) {
-            passwordHex.append(String.format("%02X", 0xFF & b));
-        }
+        String passwordHex = getSHA1Hex(password);
         for (Jogador jogador : jogadoresList) {
             if (user.equals(jogador.getUser())){
-                return passwordHex.toString().equals(jogador.getPassword());
+                return passwordHex.equals(jogador.getPassword());
+            }
+        }
+        return false;
+    }
+
+    public Boolean createJogador(Jogador jogador) throws IOException, NoSuchAlgorithmException {
+        if (verificaJogador(jogador)){
+            return false;
+        }
+        CSVWriter writer = new CSVWriter(new FileWriter(path,true));
+        jogador.setPassword(getSHA1Hex(jogador.getPassword()));
+        writer.writeNext(new String[]{jogador.getUser(), jogador.getPassword()});
+        writer.flush();
+        return true;
+    }
+
+    private String getSHA1Hex(String string) throws NoSuchAlgorithmException {
+        MessageDigest algorithm = MessageDigest.getInstance("SHA-1");
+        byte[] passwordHash = algorithm.digest(string.getBytes(StandardCharsets.UTF_8));
+        var stringHex = new StringBuilder();
+        for (byte b : passwordHash) {
+            stringHex.append(String.format("%02X", 0xFF & b));
+        }
+        return stringHex.toString();
+    }
+
+    private boolean verificaJogador(Jogador jogador){
+        if (jogador.getUser().length() < 5 || jogador.getUser().length() > 10
+                || jogador.getPassword().length() < 4 || jogador.getPassword().length() > 8){
+            return true;
+        }
+        for (int i = 0; i < jogador.getUser().length(); i++){
+            if (!Character.isAlphabetic(jogador.getUser().charAt(i))
+                    && !Character.isDigit(jogador.getUser().charAt(i))){
+                return true;
+            }
+        }
+        for (int i = 0; i < jogador.getPassword().length(); i++){
+            if (!Character.isAlphabetic(jogador.getPassword().charAt(i))
+                    && !Character.isDigit(jogador.getPassword().charAt(i))){
+                return true;
             }
         }
         return false;
